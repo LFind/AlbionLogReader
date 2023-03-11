f410a21979cd3adce9a5a355e844d2eaf0e7b816
LFind <93028040+LFind@users.noreply.github.com>
2023-03-11 17:39:26 +0700

    Update Event.gd

--- Event.gd
+++ Event.gd
@@ -1,11 +1,14 @@
 extends Node
 
-signal removed_all_entries(item:String)
-signal new_release
+
 signal update_settings(key:Settings.Key, value:Variant)
 signal update_item_settings(item_name:String)
 signal update_logs
 signal update_items
+signal removed_all_entries(item:String)
+
 signal request_window_item_info(item_name:String)
 signal request_window_item_options(item_name:String)
-signal request_window_options
+signal request_window_settings
+
+signal group_update_items(group_name:String)
