c07aa7cccbf232b97011ad34504a1e33798c4e68
LFind <93028040+LFind@users.noreply.github.com>
2023-03-11 17:45:25 +0700

    Code: Подхват цветов из конфига

--- scenes/items_table/elements/table_item.gd
+++ scenes/items_table/elements/table_item.gd
@@ -1,9 +1,5 @@
 extends HBoxContainer
 
-const COLOR_MIN:Color = Color("703030")
-const COLOR_LOW:Color = Color("823f00")
-const COLOR_NORMAL:Color = Color("363636")
-
 const SECTION = "table"
 const CONFIG_SHOW_HIDDEN = "show_hidden"
 
@@ -11,6 +7,10 @@ const CONFIG_SHOW_HIDDEN = "show_hidden"
 @onready var button_name = %ButtonName
 @onready var button_count = %ButtonCount
 @onready var panel_count = $ButtonCount/Panel
+
+@onready var color_min:Color = Settings.get_color_min()
+@onready var color_low:Color = Settings.get_color_low()
+@onready var color_default:Color = Settings.get_color_default()
 
 var entries:Dictionary
 var item:String:
@@ -28,11 +28,25 @@ func _ready():
 	button_name.pressed.connect(func():
 		Event.request_window_item_info.emit(self.item)
 		)
-	Event.update_settings.connect(func(key,value): if key == Settings.Key.SHOW_HIDDEN: _update_visibility())
+	Event.update_settings.connect(_update_settings)
 	Event.update_item_settings.connect(func(item_name): if item_name == self.item: update.emit())
 	panel_count.get_theme_stylebox("panel")
 	update.connect(_update)
 	_update()
+
+func _update_settings(key,value):
+	if key == Settings.Key.SHOW_HIDDEN:
+		_update_visibility()
+	elif visible:
+		if key == Settings.Key.COLOR_LOW:
+			color_low = Settings.get_color_low()
+			_update()
+		elif key == Settings.Key.COLOR_DEFAULT:
+			color_default = Settings.get_color_default()
+			_update()
+		elif key == Settings.Key.COLOR_MIN:
+			color_min = Settings.get_color_min()
+			_update()
 
 func _update_visibility():
 	visible = item != "" and (not ItemData.is_hidden(item) or Settings.get_show_hidden())
@@ -42,7 +56,7 @@ func _update_visibility():
 
 func _update():
 	_update_visibility()
-	if item != "":
+	if item != "" and visible:
 		self.entries = LogData.get_entries(self.item)
 		self.count = LogData.get_item_count(self.item)
 		self.last_change = LogData.find_latest_date(self.entries, self.item)
@@ -53,12 +67,12 @@ func _update():
 		texture_rect.texture =  ItemData.get_icon(self.item)
 		
 		if self.count <= ItemData.get_count_min(self.item):
-			panel_count.modulate = COLOR_MIN
+			panel_count.modulate = color_min
 		elif self.count <= ItemData.get_count_low(self.item):
-			panel_count.modulate = COLOR_LOW
+			panel_count.modulate = color_low
 		else:
-			panel_count.modulate = COLOR_NORMAL
-	
+			panel_count.modulate = color_default
+
 
 func set_item(item:String):
 	self.item = item
