46a1e4d4b846e9edf7289ca0c6a710a5e2a9a233
LFind <93028040+LFind@users.noreply.github.com>
2023-03-11 17:39:15 +0700

    Code: Мелкие измемения

--- UpdateChecker.gd
+++ UpdateChecker.gd
@@ -1,55 +1,100 @@
 extends Node
 
-const GITHUB_API = "https://api.github.com/repos/{owner}/{repo}/releases"
-const AUTHOR = "LFind"
-const REPO = "AlbionLogReader"
+const _GITHUB_API = "https://api.github.com/repos/{owner}/{repo}/releases"
+const _AUTHOR = "LFind"
+const _REPO = "AlbionLogReader"
 
-var list_releases:Array
-var latest_release:Dictionary
-var request = HTTPRequest.new()
+const STATUS_UNKNOWN = -1
+const STATUS_OLD = 0
+const STATUS_LATEST = 1
+
+var _response_code:int = -1
+var _status:int = STATUS_UNKNOWN
+var _list_releases:Array
+var _latest_release:Dictionary
+var _request = HTTPRequest.new()
+
+signal new_release
+signal search_start
+signal search_end
+signal download_start
+signal download_end
 
 func _ready():
-	add_child(request)
-	check_updates()
+	add_child(_request)
+	_request.request_completed.connect(_on_request_completed)
+	if Settings.get_search_update_on_launch():
+		check_updates()
 
 func check_updates():
-	request.request_completed.connect(_on_request_completed)
-	request.request(GITHUB_API.format({ "owner": AUTHOR, "repo": REPO }))
+	search_start.emit()
+	_request.request(_GITHUB_API.format({ "owner": _AUTHOR, "repo": _REPO }))
 
 func get_download_link() -> String:
-	if latest_release.has("assets") and latest_release["assets"].size() > 0:
-		return latest_release["assets"][0]["browser_download_url"]
+	if _latest_release.has("assets") and _latest_release["assets"].size() > 0:
+		return _latest_release["assets"][0]["browser_download_url"]
 	else:
 		return ""
 
+func get_response() -> int:
+	return _response_code
+
+func get_status() -> int:
+	return _status
+
 func get_latest_version() -> String:
-	return latest_release.get("tag_name", Settings.VERSION)
+	return _latest_release.get("tag_name", Settings.VERSION)
 
 func get_latest_release_link() -> String:
-	return latest_release.get("html_url","")
+	return _latest_release.get("html_url","")
 
 func _on_request_completed(result, response_code, headers, body):
+	_response_code = response_code
 	if response_code != 200:
+		_status = STATUS_UNKNOWN
+		_on_search_complete()
 		return
 	
-	list_releases = str_to_var(body.get_string_from_utf8())
-	latest_release = list_releases[0]
-	var latest_version = latest_release["tag_name"]
+	_list_releases = str_to_var(body.get_string_from_utf8())
+	_latest_release = _list_releases[0]
+	var latest_version = _latest_release["tag_name"]
 	
 	var compare_result = _compare_versions(latest_version, Settings.VERSION)
 	if compare_result > 0:
-		# There's a newer version available
-		var download_url = latest_release["html_url"]
-		print("Current version: v", Settings.VERSION)
-		print("New version available: ", latest_version)
-		print("Download it here: ", download_url)
-		Event.new_release.emit()
+		_status = STATUS_OLD
+		var download_url = _latest_release["html_url"]
 	elif compare_result == 0:
-		print("Current version: v", Settings.VERSION)
-		print("New version not found.")
+		_status = STATUS_LATEST
 	else:
-		printerr("Something wrong on update check!. Current version is v{curr}, but latest availble: v{last}"
-			.format({"curr": Settings.VERSION, "last": latest_version}) )
+		_status = STATUS_UNKNOWN
+	_on_search_complete()
+
+func _on_search_complete():
+	match get_status():
+		STATUS_LATEST: 
+			print("Current version: v{version}\nNew version not found".format({
+				"version": Settings.VERSION
+			}))
+		STATUS_OLD:
+			print("Current version: v{version}\nNew version available: v{new_version}\nLink: {link}"
+			.format({
+				"version": Settings.VERSION,
+				"new_version": get_latest_version(),
+				"link": get_download_link()
+			}))
+		STATUS_UNKNOWN:
+			if _response_code != 200:
+				printerr("Something wrong on update check! Response: {response}"
+				.format({
+						"response": _response_code
+				}))
+			else:
+				printerr("Something wrong on update check!. Current version is v{version}, but latest availble: v{new_version}"
+				.format({"version": Settings.VERSION,
+					"new_version": get_latest_version()
+				}))
+	
+	search_end.emit()
 
 func _compare_versions(version1: String, version2: String) -> int:
 	var v1 = version1.replace("b","").to_float()
